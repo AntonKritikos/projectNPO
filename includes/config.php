6a42d595eb4bfed4aaadcd682998a8d9bd72dc84
sirbulters <j_bulters@live.no>
2015-12-15 19:36:31 +0100

    versie 5 npo schoonmaak1

--- includes/config.php
+++ includes/config.php
@@ -1,21 +1,14 @@
 <?php
 
-// Definieer de status van het project
 define('PROJECT_STATUS','development');
 
-// Stel de instellingen in op Nederlands
 setlocale(LC_ALL, 'nl_NL');
 
-// databasegegevens
-//define('DB_HOST', '127.0.0.1');
+
 define('DB_HOST','localhost');
 define('DB_NAME','npomusic');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','root');
-
-//define('NR_ITEMS_PAGE', 5);//for pagination
-//define('NR_ITEMS_PAGE2', 20);//for pagination2 tour
-//define('NR_ITEMS_PAGE3', 3);//for pagination3 music
 
 
 ?>
